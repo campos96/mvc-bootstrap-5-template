9b1aea8b9bdecb20807f31fb2e71d8387d64b116
cesarhc96 <cesarhcmartinez@gmail.com>
2022-02-22 20:44:44 -0700

    packages updated / basic tables templates

--- mvc-bootstrap-5-template/App_Start/BundleConfig.cs
+++ mvc-bootstrap-5-template/App_Start/BundleConfig.cs
@@ -5,26 +5,22 @@ namespace mvc_bootstrap_5_template
 {
     public class BundleConfig
     {
-        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new Bundle("~/bundles/js").Include(
+                "~/Scripts/jquery-{version}.js",
+                "~/Scripts/bootstrap.bundle.js",
+                "~/Scripts/modernizr-*",
+                "~/Scripts/jquery.validate*",
+                "~/Scripts/DataTables/jquery.dataTables.js",
+                "~/Scripts/DataTables/dataTables.bootstrap5.min.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            bundles.Add(new Bundle("~/bundles/css").Include(
+                "~/Content/bootstrap.css",
+                "~/Content/site.css",
+                "~/Content/DataTables/css/jquery.dataTables.css",
+                "~/Content/DataTables/css/dataTables.bootstrap5.css",
+                "~/Content/font-awesome.css"));
         }
     }
 }
