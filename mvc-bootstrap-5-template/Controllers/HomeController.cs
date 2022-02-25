8824286746755bb9d54e99d9f18c76bb198a2a4c
cesarhc96 <cesarhcmartinez@gmail.com>
2022-02-24 21:20:14 -0700

    add simple tables

--- mvc-bootstrap-5-template/Controllers/HomeController.cs
+++ mvc-bootstrap-5-template/Controllers/HomeController.cs
@@ -13,18 +13,10 @@ namespace mvc_bootstrap_5_template.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult BasicTables()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
